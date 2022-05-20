ff8cba866b326b8364da3d5fa035bd327650f587
Takashi <th2826@columbia.edu>
2022-05-20 18:17:58 -0400

    completed assignment

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,12 @@
 class Company < ActiveRecord::Base
+  has_many :freebies
+  has_many :devs, through: :freebies
 
+  def give_freebie(dev, item_name, value)
+    Freebie.create(item_name: item_name, value: value, company: self, dev: dev)
+  end
+
+  def self.oldest_company
+    self.order(:founding_year).first
+  end
 end
--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,16 @@
 class Dev < ActiveRecord::Base
+  has_many :freebies
+  has_many :companies, through: :freebies
 
+  def received_one?(item_name)
+    self.freebies.exists?(:item_name => item_name)
+  end
+
+  def give_away(dev, freebie)
+    if self.received_one?(freebie.item_name)
+      freebie.update(dev: dev)
+    else
+      "you can't give this away because it's not yours"
+    end
+  end
 end
